da356fce6ce75c75546d1b6d959671d7f3b389b9
tschuebel <tschuebel@gmail.com>
2024-09-01 16:10:46 +0200

    Link bitcode of lodepng

--- source/file/FileSystem.cpp
+++ source/file/FileSystem.cpp
@@ -28,26 +28,26 @@ namespace spark {
 
             std::vector<uc8_t> png;						// the png file
             std::vector<uc8_t> image;					// the raw pixels
-            //uint32_t width, height, fileSize, rawSize;	// width, height and size of the png
+            uint32_t width, height, fileSize, rawSize;	// width, height and size of the png
 
             //load and decode
-            //lodepng::load_file(png, absolutePathFile);
-            //int32_t error = lodepng::decode(image, width, height, png);            
+            lodepng::load_file(png, absolutePathFile);
+            int32_t error = lodepng::decode(image, width, height, png);            
 
-            //if (error == 0)
-            //{
-            //    m_logger->info("Loading '%s' successful", fileName.c_str());
-            //    fileSize = png.size();
-            //    rawSize = image.size();
-            //    bmp = new drawing::Bitmap(&image[0], width, height, spark::drawing::E_RGBA8);
-            //}
-            //else
-            //{
-            //    m_logger->info("Loading '%s' not successful with code %i", fileName.c_str(), error);
-            //    fileSize = 0;
-            //    rawSize = 0;
-            //    bmp = NULL;
-            //}
+            if (error == 0)
+            {
+                m_logger->info("Loading '%s' successful", fileName.c_str());
+                fileSize = png.size();
+                rawSize = image.size();
+                bmp = new drawing::Bitmap(&image[0], width, height, spark::drawing::E_RGBA8);
+            }
+            else
+            {
+                m_logger->info("Loading '%s' not successful with code %i", fileName.c_str(), error);
+                fileSize = 0;
+                rawSize = 0;
+                bmp = NULL;
+            }
             return bmp;
         }
 
