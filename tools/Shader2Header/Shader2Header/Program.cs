e072840cc384e4a7933000ceb985bf83f838c1ae
tschuebel <tschuebel@gmail.com>
2024-09-29 16:27:30 +0200

    Shader2Header project added

---
+++ tools/Shader2Header/Shader2Header/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IniParser;
+using Shader2Header;
+
+Console.WriteLine("Start");
+
+var parser = new FileIniDataParser();
+//IniData data = parser.ReadFile("Shader2Header.ini");
+
+var defaultShader = new DefaultShader
+{
+    RootFolder = @"..\..\..\..\..\..\source\renderer\shader",
+    Version = "glsl-for-ogl-feature-level-glsles10",
+    ClassName = "DefaultShader",
+    VertexName = "Default.vert",
+    FragmentName = "Default.frag"
+};
+
+var converter = new ConverterCommand(defaultShader);
+converter.Execute();
