fca047d5faa4707f26714cc310e5ad4cfc8a33f3
tschuebel <tschuebel@gmail.com>
2024-11-10 23:37:25 +0100

    lodepng moved to build

--- lib/lodepng/build/lib-vc2022/build.bat
+++ lib/lodepng/build/lib-vc2022/build.bat
@@ -1,5 +1,5 @@
 :: Multithreaded-Debug-DLL (/MDd), Multithreaded-DLL (/MD), Multithreaded-Debug (/MTd), Multithreaded (/MT)
-cl /c ../lodepng.cpp /MDd
+cl /c ../../lodepng.cpp /MDd
 lib lodepng.obj /OUT:lodepngd.lib
-cl /c ../lodepng.cpp /MD
+cl /c ../../lodepng.cpp /MD
 lib lodepng.obj /OUT:lodepng.lib
