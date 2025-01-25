7a0f421d11ab1ebc99dd0a47d519e106dee23b65
tschuebel <tschuebel@gmail.com>
2025-01-25 13:53:42 +0100

    Fix include gueard

--- source/mlstl/MLHash.hpp
+++ source/mlstl/MLHash.hpp
@@ -1,34 +1,37 @@
-#ifndef __SPARK_MLSTL_BASE64_MLBASE64_HPP_INCLUDED__
-#define __SPARK_MLSTL_BASE64_MLBASE64_HPP_INCLUDED__
+#ifndef __SPARK_MLSTL_HASH_MLHASH_HPP_INCLUDED__
+#define __SPARK_MLSTL_HASH_MLHASH_HPP_INCLUDED__
+
 #include <vector>
 #include <string>
 #include <spark/SparkTypes.hpp>
 
-namespace mlstl {
-    namespace hash {
-        /**
-        * Calculates hash for the provided byte stream using Fowler-Noll-Vo algorithmus
-        */
-        class MLHash
-        {
-        public:
-            static std::string hashFNV1a(std::vector<spark::uc8_t>& data)
+namespace spark {
+    namespace mlstl {
+        namespace hash {
+            /**
+            * Calculates hash for the provided byte stream using Fowler-Noll-Vo algorithmus
+            */
+            class MLHash
             {
-                const int64_t FNV_OFFSET_BASIS = 14695981039346656037U;
-                const int64_t FNV_64_PRIME = 1099511628211U;
-
-                int64_t hash = FNV_OFFSET_BASIS;
-
-                for (auto byte : data)
+            public:
+                static std::string hashFNV1a(std::vector<spark::uc8_t>& data)
                 {
-                    hash ^= byte;            // XOR the byte with the current hash
-                    hash *= FNV_64_PRIME;    // Multiply by the FNV prime
+                    const int64_t FNV_OFFSET_BASIS = 14695981039346656037U;
+                    const int64_t FNV_64_PRIME = 1099511628211U;
+
+                    int64_t hash = FNV_OFFSET_BASIS;
+
+                    for (auto byte : data)
+                    {
+                        hash ^= byte;            // XOR the byte with the current hash
+                        hash *= FNV_64_PRIME;    // Multiply by the FNV prime
+                    }
+                    std::ostringstream oss;
+                    oss << hash;
+                    return oss.str();
                 }
-                std::ostringstream oss;
-                oss << hash;
-                return oss.str();
-            }
-        };
+            };
+        }
     }
 }
 #endif
